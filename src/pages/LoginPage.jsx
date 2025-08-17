abd72141e38e83cda580e81566b3d10243a08175
ManivasagamP <137617174+ManivasagamP@users.noreply.github.com>
2025-08-17 20:11:48 +0530

    feat: enhance Login and Profile pages with form handling and layout improvements

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,99 @@
 import React from 'react'
+import assets from '../assets/assets'
 
 const LoginPage = () => {
+  const [curreState, setCurrentState] = React.useState('signUp');
+  const [fullName, setFullName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [bio, setBio] = React.useState("");
+  const [isDataSubmitted, setIsDataSubmitted] = React.useState(false);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    
+    if(curreState === "signUp" && !isDataSubmitted){
+      setIsDataSubmitted(true);
+      return;
+    }
+  }
+
   return (
-    <h1>HII</h1>
+    <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly max-sm:flex-col backdrop-blur-2xl '>
+      {/*Left*/}
+      <img src={assets.logo_big} alt="" className='w-[min(30vw,250px)]' />
+
+      {/*Right*/}
+      <form onSubmit={onSubmitHandler} className='vorder-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg shadow-lg'>
+        <h2 className='font-mediumtest-2xl flex justify-between items-center'>
+          {curreState === "signUp" ? "Sign Up" : "Login"}
+          {isDataSubmitted && 
+          <
+            img
+            onClick={() => setIsDataSubmitted(false)}
+            src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' 
+          />}
+        </h2>
+
+        {curreState === "signUp" && !isDataSubmitted && (
+          <input
+            onChange={(e) => setFullName(e.target.value)}
+            value={fullName}
+            type="text" placeholder='Enter your Full name' className='p-2 border border-gray-500 rounded-md focus:outline-none' required
+          />
+        )}
+
+        {!isDataSubmitted && (
+          <>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email" placeholder='Enter your e-mail...' className='p-2 border border-gray-500 rounded-md focus:outline-none' required
+            />
+            <input type="password" placeholder='Enter your password...' className='p-2 border border-gray-500 rounded-md focus:outline-none' required
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+          </>
+        )}
+
+        {curreState === "signUp" && isDataSubmitted && (
+          <textArea rows={4}
+            className='p-2 border border-gray-500 rounded-md focus:outline-none'
+            placeholder='Tell us about yourself...'
+            onChange={(e) => setBio(e.target.value)}
+            value={bio}
+            required
+          >
+
+          </textArea>
+        )}
+
+        <button
+          type='submit'
+          className='py-3 bg-gradient-to-r from-purple-400 to-violet-600 text-white rounded-md cursor-pointer'>
+          {curreState === "signUp" ? "Create Account" : "Login now"}
+        </button>
+
+        <div className='flex items-center gap-2 text-sm twxt-gray-500'>
+          <input type="checkbox" />
+          <p>Agree to the terms od use & privacy policy.</p>
+        </div>
+
+        <div className='flex flex-col gap-2'>
+          {curreState === "signUp" ? (
+            <p
+              className='text-gray-500 text-sm'
+            >Already have an account? <span className='font-medium text-violet-500 cursor-pointer' onClick={() => { setCurrentState("login"); setIsDataSubmitted(false) }}>Login here</span></p>
+          ) : (
+            <p
+              className='text-gray-500 text-sm'
+            >Create an account <span className='font-medium text-violet-500 cursor-pointer' onClick={() => { setCurrentState("signUp") }}>Click here</span></p>
+          )}
+        </div>
+      </form>
+    </div>
   )
 }
 
-export default LoginPage
+export default LoginPage 
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import assets from '../assets/assets';
 
 const ProfilePage = () => {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+  const navigate = useNavigate();
+  const [name, setName] = React.useState('Anton joe');
+  const [bio, setBio] = React.useState('Hi everyone !!');
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    navigate("/")
+  }
+
   return (
-    <div>Profile Page</div>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-1 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
+          <h3 className='text-lg'>Profile Details</h3>
+          <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
+            <input onChange={(e) => setSelectedImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden/>
+            <img src={selectedImage ? URL.createObjectURL(selectedImage) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImage && "rounded-full"}`} />
+            upload profile image
+          </label>
+          <input onChange={(e) => setName(e.target.value)} value={name} type="text" required placeholder='Youe name' className='p-2 border border-gray-500 rounded-md focus:outline-none focus-ring-2 focus:ring-violet-500'/>
+          <textarea onChange={(e) => setBio(e.target.value)} value={bio} placeholder='Write profile bio' className='p-2 border border-gray-500 rounded-md focus:outline-none focus-ring-2 focus:ring-violet-500' rows={4} required></textarea>
+          <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full test-lg cursor-pointer'>Save</button>
+        </form>
+        <img src={assets.logo_icon} className="max-w-44 aspect-square rounded-full mx-10 max-sm:mt-10" alt="" />
+      </div>
+    </div>
   )
 }
 
